fcc521a15665b1bb50401abc07fba4db2aaf9951
CaiooSouzaa <caioggsouza@hotmail.com>
2025-07-27 20:48:03 -0300

    new commit

---
+++ index.js
@@ -0,0 +1,86 @@
+let altura = 0
+let largura = 0
+let vidas = 1
+
+function ajustarTamanhopalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+
+ajustarTamanhopalcoJogo()
+
+
+//Criar o elemento HTML, randomizando e removendo
+function posicaoRandomica() {
+
+    //removendo
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+
+        if (vidas > 3) {
+            alert('Interromper o jogo')
+        } else {
+            document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
+
+            vidas++
+        }
+    }
+
+
+    //randomizando
+    let posicaoX = Math.floor(Math.random() * largura) - 90
+    let posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //criando elemento
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio() //aqui eu eparao as string fazendo com que seja reconhecida cada parte separada e não tenha conflitos no css
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function () {
+        this.remove()
+    }
+
+    document.body.appendChild(mosquito)
+
+    console.log()
+}
+
+//tamanho aleatorio
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3);
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+
+    console.log(classe)
+}
+
+//Lado aleatorio
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2);
+
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
+}
